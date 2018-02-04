a9405a5d1d770c0aad97d9023297412292c6743b
DESKTOP-9ITJT22\iiand <iian.d.neill@gmail.com>
2018-02-04 15:12:02 +1000

    Work on admin controllers

---
+++ Codex-RI/Codex-RI/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Codex_RI.Areas.Admin.Controllers
+{
+    public class AdminBaseController : Codex_RI.Controllers.BaseController
+    {
+
+    }
+}
---
+++ Codex-RI/Codex-RI/Areas/Admin/Controllers/PersonController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Codex_RI.Areas.Admin.Controllers
+{
+    public class PersonController : AdminBaseController
+    {
+        [HttpGet]
+        public ActionResult Index(Guid guid)
+        {
+            return View();
+        }
+    }
+}
